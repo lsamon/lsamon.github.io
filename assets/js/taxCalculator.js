19ff132dd92368daf86a234e0b2b6e3f40279dc6
lsamon <louis.amon@amazutech.net>
2017-02-09 00:40:21 +1100

    Tax Calculator functionality added JQuery

---
+++ assets/js/taxCalculator.js
@@ -0,0 +1,52 @@
+var payments = [];
+var total;
+const NSSF = 0.1
+
+var taxOnIncome = function(amount, rate){
+  return amount * rate;
+}
+
+var taxOnNSSF = function(amount){
+  return amount * NSSF;
+}
+
+var calcTax = function(amount){
+ var tax = 0;
+    if(amount > 720000){
+      tax = taxOnIncome(amount, 0.3);
+    }
+    else if(amount > 540000){
+      tax = taxOnIncome(amount, 0.25);
+    }
+    else if( amount > 360000){
+      tax = taxOnIncome(amount, 0.2);
+    }
+    else if(amount > 170000){
+      tax = taxOnIncome(amount, 0.09);
+    }
+    else{
+      tax = taxOnIncome(amount, 0);
+    }
+
+    return tax;
+}
+
+$(document).ready(function(){
+  $(".calc-form").on('submit', function (e) {
+    e.preventDefault();
+    var income = $('#tax-amount').val();
+
+    var taxAmount = calcTax(income) + taxOnNSSF(income);
+    var netIncome = income - taxAmount;
+
+    console.log(taxAmount);
+    console.log(netIncome);
+
+    $('.base-income').text(income);
+    $('.tax-on-income').text(calcTax(income));
+    $('.social-security').text(taxOnNSSF(income));
+    $('.total-tax').text(taxAmount);
+    $('.net-income').text(income - taxAmount);
+
+  });
+})
