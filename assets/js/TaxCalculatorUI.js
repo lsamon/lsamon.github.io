1edf56afc2017a67ac7afaf496b6ba88eb1346da
Louis Amon <louis.amon@amazutech.net>
2020-12-30 08:44:20 +1100

    Add event listeners and error handler for input field

--- assets/js/TaxCalculatorUI.js
+++ assets/js/TaxCalculatorUI.js
@@ -3,11 +3,24 @@ import TaxCalculator from './TaxCalculator.js';
 let taxCalculator = new TaxCalculator();
 const workersCompensation = 0.01;
 const skillsDevelopment = 0.05;
+let commaPosition = 4;
+let taxableAmountArr = [];
 
 function grossIncome() {
-  const el = document.querySelector('#tax-amount');
+  const el = document.querySelector('#taxable-amount');
   return el.value == '' ? 0 : parseInt(el.value);
 }
+
+document.querySelector('#taxable-amount').addEventListener('blur', () => {
+  taxCalculator.grossIncome = grossIncome();
+});
+
+document.querySelector('#taxable-amount').addEventListener('keyup', () => {
+  if (document.querySelector('input#taxable-amount').classList.contains('invalid-input')) {
+    document.querySelector('.taxable-amount-invalid').classList.add('hidden');
+    document.querySelector('input#taxable-amount').classList.remove('invalid-input');
+  }
+});
 
 document.querySelectorAll('.radio').forEach((item) => {
   item.addEventListener('click', () => {
@@ -16,14 +29,19 @@ document.querySelectorAll('.radio').forEach((item) => {
     taxCalculator.employeeDeductions = parseFloat(item.dataset.employeeDeductions);
     taxCalculator.employerDeductions = parseFloat(item.dataset.employerDeductions);
     taxCalculator.socialSecurityDeduction = parseFloat(item.dataset.socialSecurityDeduction);
-
-    let socialSecurityAmount = taxCalculator.otherDeductions(taxCalculator.socialSecurityDeduction);
-    taxCalculator.grossIncome = grossIncome() - socialSecurityAmount;
   });
 });
 
 document.querySelector('#calculate-tax').addEventListener('click', (e) => {
   e.preventDefault();
+  if (document.querySelector('#taxable-amount').value === "") {
+    document.querySelector('.taxable-amount-invalid').classList.remove('hidden');
+    document.querySelector('input#taxable-amount').classList.add('invalid-input');
+    return;
+  }
+
+  let socialSecurityAmount = taxCalculator.otherDeductions(taxCalculator.socialSecurityDeduction);
+  taxCalculator.grossIncome = taxCalculator.grossIncome - socialSecurityAmount;
 
   document.querySelector('.calc-result').classList.remove('hidden');
   document.querySelector('.base-income').textContent = grossIncome();
@@ -44,5 +62,5 @@ document.querySelector('#clear-values').addEventListener('click', (e) => {
   e.preventDefault();
   document.querySelectorAll('.radio').forEach((button) => { button.classList.remove('clicked') });
   document.querySelector('.calc-result').classList.add('hidden');
-  document.querySelector('#tax-amount').value = '';
+  document.querySelector('#taxable-amount').value = '';
 });
